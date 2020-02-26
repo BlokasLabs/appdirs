5fd61e4e4f377a5cb01ab02594e20faf4b04a04e
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2020-02-26 20:36:21 +0100

    New upstream version 1.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@
 import sys
 import os
 import os.path
-from setuptools import setup
+# appdirs is a dependency of setuptools, so allow installing without it.
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 import appdirs
 
 tests_require = []
@@ -20,8 +24,8 @@ def read(fname):
 setup(
     name='appdirs',
     version=appdirs.__version__,
-    description='A small Python module for determining appropriate " + \
-        "platform-specific dirs, e.g. a "user data dir".',
+    description='A small Python module for determining appropriate ' + \
+        'platform-specific dirs, e.g. a "user data dir".',
     long_description=read('README.rst') + '\n' + read('CHANGES.rst'),
     classifiers=[c.strip() for c in """
         Development Status :: 4 - Beta
@@ -29,13 +33,16 @@ setup(
         License :: OSI Approved :: MIT License
         Operating System :: OS Independent
         Programming Language :: Python :: 2
-        Programming Language :: Python :: 2.4
-        Programming Language :: Python :: 2.5
         Programming Language :: Python :: 2.6
         Programming Language :: Python :: 2.7
         Programming Language :: Python :: 3
-        Programming Language :: Python :: 3.1
         Programming Language :: Python :: 3.2
+        Programming Language :: Python :: 3.3
+        Programming Language :: Python :: 3.4
+        Programming Language :: Python :: 3.5
+        Programming Language :: Python :: 3.6
+        Programming Language :: Python :: Implementation :: PyPy
+        Programming Language :: Python :: Implementation :: CPython
         Topic :: Software Development :: Libraries :: Python Modules
         """.split('\n') if c.strip()],
     test_suite='test.test_api',
@@ -43,8 +50,8 @@ setup(
     keywords='application directory log cache user',
     author='Trent Mick',
     author_email='trentm@gmail.com',
-    maintainer='Trent Mick; Sridhar Ratnakumar',
-    maintainer_email='trentm@gmail.com; github@srid.name',
+    maintainer='Trent Mick; Sridhar Ratnakumar; Jeff Rouse',
+    maintainer_email='trentm@gmail.com; github@srid.name; jr@its.to',
     url='http://github.com/ActiveState/appdirs',
     license='MIT',
     py_modules=["appdirs"],
